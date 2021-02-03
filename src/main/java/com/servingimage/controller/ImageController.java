9b66e3920782bca456db25c451a5c5baaac56c1c
ali2coder <almowins5yearsss@gmail.com>
2021-02-03 12:13:45 +0900

    update

--- src/main/java/com/servingimage/controller/ImageController.java
+++ src/main/java/com/servingimage/controller/ImageController.java
@@ -5,33 +5,44 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.Charset;
+
 
 @Controller
+
 public class ImageController {
 
 
     // Directly writing to HttpServletResponse
-    @RequestMapping(value = "/apple", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    public void getImage1(HttpServletResponse response) throws IOException {
-        Resource image = new ClassPathResource("images/fruits/APPLE.jpg");
+    @RequestMapping(value = "/image/{name}",
+            method = RequestMethod.GET,
+            produces = MediaType.IMAGE_JPEG_VALUE)
+    public void getImage(HttpServletResponse response,
+                          @PathVariable(value = "name") String imageName) throws IOException {
+
+        // First step is to bring an image using the path variable imageName
+
+        Resource image = new ClassPathResource("images/fruits/apple.jpg");
         response.setContentType(MediaType.IMAGE_JPEG_VALUE);
         StreamUtils.copy(image.getInputStream(), response.getOutputStream());
     }
 
     // Using ResponseEntity
-    @RequestMapping(value = "/peach", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> getImage2() throws IOException{
-        var image = new FileUrlResource("/home/ali/Pictures/PEACH.jpg");
+    @RequestMapping(value = "/images/{name}",
+            method = RequestMethod.GET,
+            produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage(@PathVariable String name) throws IOException{
+        String basePath = "/home/some_user/storage/";
+        var image = new FileUrlResource(basePath+name+".jpg");
 
 //        These are also valid
-//        var image = new FileSystemResource("/home/ali/Pictures/PEACH.jpg");
-//        var image = new UrlResource("file:////home/ali/Pictures/PEACH.jpg");
+//        var image = new FileSystemResource("/home/some_user/storage/apple.jpg");
+//        var image = new UrlResource("file:////home/some_user/storage/peach.jpg");
 
         System.out.println(image.exists());
         byte[] bytes = StreamUtils.copyToByteArray(image.getInputStream());
@@ -43,11 +54,12 @@ public class ImageController {
 
     }
     // Using StreamInputResource
-    @RequestMapping(value = "/pomegranate", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<InputStreamResource> getImage3() throws IOException {
+    @RequestMapping(value = "/images/{name}",
+            method = RequestMethod.GET,
+            produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<InputStreamResource> getImage1(@PathVariable String name) throws IOException {
 
-        var image = new ClassPathResource("images/fruits/POMEGRANATE.jpg");
-
+        var image = new ClassPathResource("images/fruits/" + name + ".jpg");
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.IMAGE_JPEG)
